574b9e1ea72bfd4c7169cfdee4dc13146b75f5db
Cody Dupuis <codydupuis99@gmail.com>
2020-07-28 23:16:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,15 @@
-
-def unsafe?(speed)
-if (speed < 40) || (speed > 60)
-   true
- else 
-   false
+ def unsafe?(speed)
+  if speed > 60 || speed < 40
+    true
+  else 
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-  (speed < 40) || (speed > 60) ? true : false 
-	
-end
-	
 
+	speed > 60 || speed < 40 ? true : false
+end
 
